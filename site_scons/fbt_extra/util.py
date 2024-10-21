16934b2040aa5b544ce8af994b597d2af3e4491c
hedger <hedger@nanode.su>
2024-10-21 21:51:47 +0100

    fbt: better way to link 'latest' build dir

--- site_scons/fbt_extra/util.py
+++ site_scons/fbt_extra/util.py
@@ -3,11 +3,9 @@ from fbt.util import link_dir
 from SCons.Errors import StopError
 
 
-def link_elf_dir_as_latest(env, elf_node):
-    elf_dir = elf_node.Dir(".")
-    latest_dir = env.Dir("#build/latest")
-    print(fg.green(f"Linking {elf_dir} as latest built dir (./build/latest/)"))
-    return link_dir(latest_dir.abspath, elf_dir.abspath, env["PLATFORM"] == "win32")
+def link_dirs(env, *, dst, src):
+    print(fg.green(f"Linking {src} as {dst}"))
+    return link_dir(dst.abspath, src.abspath, env["PLATFORM"] == "win32")
 
 
 def should_gen_cdb_and_link_dir(env, requested_targets):
